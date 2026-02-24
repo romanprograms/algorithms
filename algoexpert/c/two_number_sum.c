e246628a6c04f765cb2972306bc22842128cf8a7
Roman Nikolaenkov <romanliveshere@gmail.com>
2026-02-23 21:46:48 -0800

    refactor two number sum

--- algoexpert/c/two_number_sum.c
+++ algoexpert/c/two_number_sum.c
@@ -1,46 +1,3 @@
-#include <stdlib.h>
-#include <stdio.h>
-#include <stdbool.h>
-#include "uthash.h"
-
-#define ARRAY_LEN(x) (sizeof(x) / sizeof((x)[0]))
-
-void printArray(int *array, size_t length)
-{
-  printf("\nArray: [");
-  for (size_t i = 0; i < length; i++)
-  {
-    printf("%d ", array[i]);
-  }
-  printf("] ");
-}
-
-typedef struct SeenNumberHashItem
-{
-  int key;
-  UT_hash_handle hh;
-} SeenNumberHashItem;
-
-bool add_num(SeenNumberHashItem **table, int key)
-{
-  SeenNumberHashItem *new_item;
-
-  HASH_FIND_INT(*table, &key, new_item);
-
-  if (!new_item)
-  {
-    new_item = malloc(sizeof(SeenNumberHashItem));
-    if (!new_item)
-    {
-      return false;
-    }
-    new_item->key = key;
-    HASH_ADD_INT(*table, key, new_item);
-    return true;
-  }
-  return false;
-}
-
 /*
  * Two Number Sum
  *
@@ -51,49 +8,91 @@ bool add_num(SeenNumberHashItem **table, int key)
  * an empty array.
  *
  */
-int *get_two_number_sum(int *array, size_t length, int targetSum)
-{
-  struct SeenNumberHashItem *table = NULL;
 
-  struct SeenNumberHashItem *new_item;
+#include "uthash.h"
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-  for (size_t i = 0; i < length; i++)
-  {
-    int target_number = targetSum - array[i];
-    HASH_FIND_INT(table, &target_number, new_item);
-    if (new_item)
-    {
-      int current_number = array[i];
-      int *answer = malloc(2 * sizeof(int));
-      if (!answer)
-      {
-        return NULL;
-      }
-      answer[0] = current_number;
-      answer[1] = target_number;
-      return answer;
-    }
+#define ARRAY_LEN(x) (sizeof(x) / sizeof((x)[0]))
 
-    add_num(&table, array[i]);
-  }
+typedef struct SeenNumber {
+  int key;           // the number we’ve seen
+  UT_hash_handle hh; // makes this struct hashable
+} SeenNumber;
 
-  return NULL;
+/* Insert key into the hash table if it isn't already there.
+   Returns true on success, false on allocation failure. */
+static bool seen_insert(SeenNumber **table, int key) {
+  SeenNumber *item = NULL;
+  HASH_FIND_INT(*table, &key, item);
+  if (item)
+    return true; // already present => still “success”
+
+  item = (SeenNumber *)malloc(sizeof(*item));
+  if (!item)
+    return false;
+
+  item->key = key;
+  HASH_ADD_INT(*table, key, item);
+  return true;
 }
 
-void two_number_sum()
-{
+/* Free all items in the hash table and set head to NULL. */
+static void seen_free_all(SeenNumber **table) {
+  SeenNumber *cur = NULL, *tmp = NULL;
+  HASH_ITER(hh, *table, cur, tmp) {
+    HASH_DEL(*table, cur);
+    free(cur);
+  }
+  *table = NULL;
+}
 
+/* Returns true and writes the pair into out[2] if found.
+   Returns false if not found OR if allocation failed (in which case no result).
+ */
+bool two_number_sum(const int *array, size_t length, int targetSum,
+                    int out[2]) {
+  SeenNumber *table = NULL;
+  bool found = false;
+
+  for (size_t i = 0; i < length; i++) {
+    int current = array[i];
+    int complement = targetSum - current;
+
+    SeenNumber *hit = NULL;
+    HASH_FIND_INT(table, &complement, hit);
+    if (hit) {
+      out[0] = current;
+      out[1] = complement;
+      found = true;
+      goto cleanup;
+    }
+
+    if (!seen_insert(&table, current)) {
+      // allocation failure
+      found = false;
+      goto cleanup;
+    }
+  }
+
+cleanup:
+  seen_free_all(&table);
+  return found;
+}
+
+/* Demo */
+int main(void) {
   int numArray[] = {3, 5, -4, 8, 11, 1, -1, 6};
   size_t length = ARRAY_LEN(numArray);
-
   int targetSum = 10;
-  int *answer = get_two_number_sum(numArray, length, targetSum);
 
-  printf("Answer:\n");
-
-  if (answer)
-  {
-    printf("[%d, %d]\n", answer[0], answer[1]);
-    free(answer);
+  int ans[2];
+  if (two_number_sum(numArray, length, targetSum, ans)) {
+    printf("[%d, %d]\n", ans[0], ans[1]);
+  } else {
+    printf("No pair found (or allocation failed).\n");
   }
+
+  return 0;
 }
