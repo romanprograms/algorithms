3a395c8744f85f2d4af72d0a451a14b2f8099fe9
Roman Nikolaenkov <romanliveshere@gmail.com>
2026-02-24 22:36:48 -0800

    pathetic first attempt at creating a vector which failed

--- data_structures/c/main.c
+++ data_structures/c/main.c
@@ -1,64 +1,6 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-typedef struct Entry {
-  char *key;
-  char *value;
-} Entry;
-
-typedef struct LookupTable {
-  size_t count;
-  size_t capacity;
-  Entry *entries;
-} LookupTable;
-
-LookupTable table;
-
-void lt_init(LookupTable *table) {
-  table->count = 0;
-  table->capacity = 4;
-  table->entries = malloc(table->capacity * sizeof(Entry));
-}
-
-void lt_add(LookupTable *table, const char *key, const char *value) {
-  if (table->count >= table->capacity) {
-    table->capacity *= 2;
-    table->entries = realloc(table->entries, table->capacity * sizeof(Entry));
-  }
-
-  table->entries[table->count].key = strdup(key);
-  table->entries[table->count].value = strdup(value);
-  table->count++;
-}
-
-const char *lt_get(LookupTable *table, const char *key) {
-  for (size_t i = 0; i < table->count; i++) {
-    if (strcmp(table->entries[i].key, key) == 0) {
-      return table->entries[i].value;
-    }
-  }
-  return NULL;
-}
-
-void lt_free(LookupTable *table) {
-  for (size_t i = 0; i < table->count; i++) {
-    free(table->entries[i].key);
-    free(table->entries[i].value);
-  }
-  free(table->entries);
-}
-
-int main() {
-  LookupTable table;
-  lt_init(&table);
-
-  lt_add(&table, "name", "Roman");
-  lt_add(&table, "language", "C");
-  lt_add(&table, "engine", "raylib");
-
-  printf("name: %s\n", lt_get(&table, "name"));
-  printf("engine: %s\n", lt_get(&table, "engine"));
-
-  lt_free(&table);
+int main(void) {
+  printf("Hello world\n");
+  return 0;
 }
---
+++ data_structures/c/vector.c
@@ -0,0 +1,66 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Vector {
+  size_t capacity;
+  size_t length;
+  int *array;
+
+} Vector;
+
+bool vector_new(Vector *v, size_t capacity) {
+  v->capacity = capacity;
+  v->length = 0;
+  v->array = malloc(capacity * sizeof(int));
+
+  if (v->array) {
+    return false;
+  }
+  return true;
+}
+
+bool vector_add(Vector *v, int element) {
+  if (v->length == v->capacity) {
+    int new_capacity = v->capacity * 2;
+    // maybe use realloc ?
+    int *new_array = malloc(new_capacity);
+    if (!new_array) {
+      return false;
+    }
+    v->capacity = new_capacity;
+    // copy existing array into new int* array
+    for (int i = 0; i < v->length; i++) {
+      new_array[i] = v->array[i];
+    }
+    // free the old array
+    free(v->array);
+    v->array = new_array;
+  }
+
+  if (v->length < v->capacity) {
+    v->array[v->length] = element;
+    v->length++;
+  }
+
+  return true;
+}
+
+void vector_print(Vector *v) {
+  for (size_t i = 0; i < v->length; i++) {
+    printf("%d", v->array[i]);
+  }
+}
+
+int main(void) {
+  Vector v;
+  bool res = vector_new(&v, 5);
+  if (!res)
+    return 1;
+
+  vector_add(&v, 2);
+  vector_print(&v);
+
+  printf("\n");
+  return 0;
+}
