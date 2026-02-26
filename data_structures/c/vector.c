608a9a6f6b2feeb71611d7ad979e34b9161aceaa
Roman Nikolaenkov <romanliveshere@gmail.com>
2026-02-26 14:10:48 -0800

    refactor vector

--- data_structures/c/vector.c
+++ data_structures/c/vector.c
@@ -4,6 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define TRYV(vecptr, expr)                                                     \
+  do {                                                                         \
+    if (!(expr)) {                                                             \
+      vector_free(vecptr);                                                     \
+      return 1;                                                                \
+    }                                                                          \
+  } while (0)
+
 typedef struct Vector {
   size_t capacity;
   size_t length;
@@ -12,6 +20,9 @@ typedef struct Vector {
 } Vector;
 
 bool vector_new(Vector *v, size_t capacity) {
+  if (!v)
+    return false;
+
   if (capacity == 0) {
     v->capacity = 1;
   } else {
@@ -27,33 +38,22 @@ bool vector_new(Vector *v, size_t capacity) {
 }
 
 bool vector_add(Vector *v, int element) {
+  if (!v)
+    return false;
+
   if (v->length == v->capacity) {
     printf("Trying to insert %d. Capacity reached. Making new allocation!\n",
            element);
 
-    if (v->capacity > SIZE_MAX / 2)
-      return false;
-
-    size_t new_capacity = v->capacity * 2;
-    // guard against overflowing size_t and allocating too small a buffer
-    // buffer overflow later.
+    size_t new_capacity = v->capacity ? v->capacity * 2 : 1;
     if (new_capacity > SIZE_MAX / sizeof *v->array)
       return false;
 
-    // maybe use realloc ?
-    int *new_array = malloc(new_capacity * sizeof(*v->array));
-
-    if (!new_array) {
+    int *tmp = realloc(v->array, new_capacity * sizeof *v->array);
+    if (!tmp)
       return false;
-    }
 
-    // copy existing array into new int* array
-    for (size_t i = 0; i < v->length; i++) {
-      new_array[i] = v->array[i];
-    }
-    // free the old array
-    free(v->array);
-    v->array = new_array;
+    v->array = tmp;
     v->capacity = new_capacity;
   }
 
@@ -89,26 +89,11 @@ int main(void) {
   if (!res)
     return 1;
 
-  if (!vector_add(&v, 1)) {
-    vector_free(&v);
-    return 1;
-  }
-  if (!vector_add(&v, 2)) {
-    vector_free(&v);
-    return 1;
-  }
-  if (!vector_add(&v, 3)) {
-    vector_free(&v);
-    return 1;
-  }
-  if (!vector_add(&v, 4)) {
-    vector_free(&v);
-    return 1;
-  }
-  if (!vector_add(&v, 5)) {
-    vector_free(&v);
-    return 1;
-  }
+  TRYV(&v, vector_add(&v, 1));
+  TRYV(&v, vector_add(&v, 2));
+  TRYV(&v, vector_add(&v, 3));
+  TRYV(&v, vector_add(&v, 4));
+  TRYV(&v, vector_add(&v, 5));
 
   printf("Printing v ");
 
