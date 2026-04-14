2570138051e4337af7b0208b69c5def156311d3a
Roman Nikolaenkov <romanliveshere@gmail.com>
2026-04-13 21:45:23 -0700

    algoexperts java two-sum solution 1

---
+++ algoexpert/java/src/main/java/org/example/TwoNumberSum.java
@@ -0,0 +1,36 @@
+package org.example;
+
+import java.util.ArrayList;
+
+public class TwoNumberSum {
+    private int[] array;
+    private int targetSum;
+
+    TwoNumberSum(int[] array, int targetSum) {
+        this.array = array;
+        this.targetSum = targetSum;
+    }
+
+    public int[] solutionOne(int [] array, int targetSum) {
+        // O(n^2) time |  O(n) space
+        for (var i = 0; i < array.length - 1 ; i++) {
+            var targetNum  = targetSum - array[i];
+            for (var j = i + 1; j < array.length; j++) {
+                if (array[j] == targetNum)  {
+                    return new int[]{array[i], array[j]};
+                }
+            }
+        }
+        return new int[0];
+    }
+
+    public void solutionTwo() {
+    }
+    public void solutionThree() {
+    }
+
+    public void run() {
+      var result =  solutionOne(array, targetSum);
+      System.out.println("Result is " + result[0] + ", " + result[1]);
+    }
+}
