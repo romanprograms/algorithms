96829119397b5999f48f087ac311796ed34a2854
Roman Nikolaenkov <romanliveshere@gmail.com>
2026-04-14 23:13:58 -0700

    algoexperts java two-sum solution 3 sort the array first

--- algoexpert/java/src/main/java/org/example/TwoNumberSum.java
+++ algoexpert/java/src/main/java/org/example/TwoNumberSum.java
@@ -1,6 +1,6 @@
 package org.example;
 
-import java.util.HashMap;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -13,12 +13,12 @@ public class TwoNumberSum {
         this.targetSum = targetSum;
     }
 
-    public int[] solutionOne(int [] array, int targetSum) {
+    public int[] solution1(int[] array, int targetSum) {
         // O(n^2) time |  O(1) space
-        for (var i = 0; i < array.length - 1 ; i++) {
-            var targetNum  = targetSum - array[i];
+        for (var i = 0; i < array.length - 1; i++) {
+            var targetNum = targetSum - array[i];
             for (var j = i + 1; j < array.length; j++) {
-                if (array[j] == targetNum)  {
+                if (array[j] == targetNum) {
                     return new int[]{array[i], array[j]};
                 }
             }
@@ -26,11 +26,11 @@ public class TwoNumberSum {
         return new int[0];
     }
 
-    public int[] solutionTwo(int [] array, int targetSum) {
+    public int[] solution2(int[] array, int targetSum) {
         // O(n) time | O(n) space
         Set<Integer> nums = new HashSet<>();
 
-        for (int num: array) {
+        for (int num : array) {
             int potentialMatch = targetSum - num;
             if (nums.contains(potentialMatch)) {
                 return new int[]{potentialMatch, num};
@@ -42,11 +42,33 @@ public class TwoNumberSum {
         return new int[0];
     }
 
-    public void solutionThree() {
+    public int[] solution3(int[] array, int targetSum) {
+        Arrays.sort(array);
+        var left = 0;
+        var right = array.length - 1;
+        while(left < right) {
+           var sum = array[left]  + array[right];
+           if (sum == targetSum) return new int[] {array[left], array[right]};
+           if (sum > targetSum) {
+               right--;
+           } else {
+               left++;
+           }
+        }
+
+        return new int[0];
+    }
+
+    public void printResults(int[]array, String name) {
+        System.out.println("result " + name + ": " + Arrays.toString(array));
     }
 
     public void run() {
-      var result =  solutionOne(array, targetSum);
-      System.out.println("Result is " + result[0] + ", " + result[1]);
+        System.out.println("Array: " + array);
+        System.out.println("targetSum: " + targetSum);
+
+        printResults(solution1(array, targetSum), "1");
+        printResults(solution2(array, targetSum), "2");
+        printResults(solution3(array, targetSum), "3");
     }
 }
